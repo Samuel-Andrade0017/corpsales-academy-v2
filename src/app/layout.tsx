9dde23de2f05822f4c99b0779501da19f9d6a522
Samuel-Andrade0017 <andrade.samuell017@gmail.com>
2026-04-19 10:37:10 -0300

    feat: clerk pt-BR localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
+import { ptBR } from '@clerk/localizations'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="pt-BR">
         <body className={inter.className}>{children}</body>
       </html>
